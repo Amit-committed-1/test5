21783d5b2c4ee790e879bc2515aa45a32553e255
Amit Kumar Bhanjbabu <amitbhanj76@gmail.com>
2025-03-13 12:18:56 +0530

    defectfix-3456-login-error

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test5Application {
-
+	int non_static = 100;
 	public static void main(String[] args) {
 		SpringApplication.run(Test5Application.class, args);
 	}
